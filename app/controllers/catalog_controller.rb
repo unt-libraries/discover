518a10034e2c69850bbfa594824b976bc4a50472
jason-ellis <jason.ellis@unt.edu>
2019-05-02 16:33:01 -0500

    Controller and model changes for our solr index

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -2,6 +2,14 @@
 class CatalogController < ApplicationController
 
   include Blacklight::Catalog
+  include Blacklight::Marc::Catalog
+
+
+  def search_action_url options = {}
+    options[:protocol] = request.headers['X-Forwarded-Proto'] or request.protocol
+    super options
+  end
+
 
   configure_blacklight do |config|
     ## Class for sending and receiving requests from a search index
@@ -18,6 +26,7 @@ class CatalogController < ApplicationController
 
     ## Default parameters to send to solr for all search-like requests. See also SearchBuilder#processed_parameters
     config.default_solr_params = {
+      qt: 'catalog-search',
       rows: 10
     }
 
@@ -29,8 +38,8 @@ class CatalogController < ApplicationController
     #config.per_page = [10,20,50,100]
 
     # solr field configuration for search results/index views
-    config.index.title_field = 'title_tsim'
-    #config.index.display_type_field = 'format'
+    config.index.title_field = 'full_title'
+    config.index.display_type_field = 'material_type'
     #config.index.thumbnail_field = 'thumbnail_path_ss'
 
     config.add_results_document_tool(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
@@ -48,8 +57,8 @@ class CatalogController < ApplicationController
     config.add_nav_action(:search_history, partial: 'blacklight/nav/search_history')
 
     # solr field configuration for document/show views
-    #config.show.title_field = 'title_tsim'
-    #config.show.display_type_field = 'format'
+    config.show.title_field = 'full_title'
+    config.show.display_type_field = 'material_type'
     #config.show.thumbnail_field = 'thumbnail_path_ss'
 
     # solr fields that will be treated as facets by the blacklight application
@@ -76,22 +85,17 @@ class CatalogController < ApplicationController
     #  (useful when user clicks "more" on a large facet and wants to navigate alphabetically across a large set of results)
     # :index_range can be an array or range of prefixes that will be used to create the navigation (note: It is case sensitive when searching values)
 
-    config.add_facet_field 'format', label: 'Format'
-    config.add_facet_field 'pub_date_ssim', label: 'Publication Year', single: true
-    config.add_facet_field 'subject_ssim', label: 'Topic', limit: 20, index_range: 'A'..'Z'
-    config.add_facet_field 'language_ssim', label: 'Language', limit: true
-    config.add_facet_field 'lc_1letter_ssim', label: 'Call Number'
-    config.add_facet_field 'subject_geo_ssim', label: 'Region'
-    config.add_facet_field 'subject_era_ssim', label: 'Era'
-
-    config.add_facet_field 'example_pivot_field', label: 'Pivot Field', :pivot => ['format', 'language_ssim']
-
-    config.add_facet_field 'example_query_facet_field', label: 'Publish Date', :query => {
-       :years_5 => { label: 'within 5 Years', fq: "pub_date_ssim:[#{Time.zone.now.year - 5 } TO *]" },
-       :years_10 => { label: 'within 10 Years', fq: "pub_date_ssim:[#{Time.zone.now.year - 10 } TO *]" },
-       :years_25 => { label: 'within 25 Years', fq: "pub_date_ssim:[#{Time.zone.now.year - 25 } TO *]" }
-    }
-
+    config.add_facet_field 'material_type', label: 'Material Type'
+    config.add_facet_field 'publication_dates', label: 'Publication Year'
+    config.add_facet_field 'genre_terms_facet', label: 'Genre'
+    config.add_facet_field 'topic_terms_facet', label: 'Topic', limit: 20, index_range: 'A'..'Z'
+    config.add_facet_field 'languages', label: 'Language', limit: true
+    config.add_facet_field 'geographic_terms_facet', label: 'Region'
+    config.add_facet_field 'era_terms_facet', label: 'Era'
+    config.add_facet_field 'form_terms_facet', label: 'Form'
+    config.add_facet_field 'people_facet', label: 'People', limit: 20
+    config.add_facet_field 'corporations_facet', label: 'Corporations', limit: 20
+    config.add_facet_field 'meetings_facet', label: 'Meetings', limit: 20
 
     # Have BL send all facet field names to Solr, which has been the default
     # previously. Simply remove these lines if you'd rather use Solr request
@@ -100,32 +104,30 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display
-    config.add_index_field 'title_tsim', label: 'Title'
-    config.add_index_field 'title_vern_ssim', label: 'Title'
-    config.add_index_field 'author_tsim', label: 'Author'
-    config.add_index_field 'author_vern_ssim', label: 'Author'
-    config.add_index_field 'format', label: 'Format'
-    config.add_index_field 'language_ssim', label: 'Language'
-    config.add_index_field 'published_ssim', label: 'Published'
-    config.add_index_field 'published_vern_ssim', label: 'Published'
-    config.add_index_field 'lc_callnum_ssim', label: 'Call number'
+    config.add_index_field 'full_title', label: 'Title'
+    config.add_index_field 'creator', label: 'Author/Creator'
+    config.add_index_field 'contributors', label: 'Contributors'
+    config.add_index_field 'material_type', label: 'Material Type'
+    config.add_index_field 'languages', label: 'Languages'
+    config.add_index_field 'publishers', label: 'Publisher'
+    config.add_index_field 'publication_places', label: 'Publication Place'
+    config.add_index_field 'publication_dates', label: 'Publication Date'
+    config.add_index_field 'main_call_number', label: 'Call number'
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
-    config.add_show_field 'title_tsim', label: 'Title'
-    config.add_show_field 'title_vern_ssim', label: 'Title'
-    config.add_show_field 'subtitle_tsim', label: 'Subtitle'
-    config.add_show_field 'subtitle_vern_ssim', label: 'Subtitle'
-    config.add_show_field 'author_tsim', label: 'Author'
-    config.add_show_field 'author_vern_ssim', label: 'Author'
-    config.add_show_field 'format', label: 'Format'
-    config.add_show_field 'url_fulltext_ssim', label: 'URL'
-    config.add_show_field 'url_suppl_ssim', label: 'More Information'
-    config.add_show_field 'language_ssim', label: 'Language'
-    config.add_show_field 'published_ssim', label: 'Published'
-    config.add_show_field 'published_vern_ssim', label: 'Published'
-    config.add_show_field 'lc_callnum_ssim', label: 'Call number'
-    config.add_show_field 'isbn_ssim', label: 'ISBN'
+    config.add_show_field 'full_title', label: 'Title'
+    config.add_show_field 'creator', label: 'Author/Creator'
+    config.add_show_field 'contributors', label: 'Contributors'
+    config.add_show_field 'material_type', label: 'Material Type'
+    config.add_show_field 'urls', label: 'URLs'
+    # config.add_show_field 'url_suppl_display', label: 'More Information'
+    config.add_show_field 'languages', label: 'Languages'
+    config.add_show_field 'publishers', label: 'Publisher'
+    config.add_show_field 'publication_places', label: 'Publication Place'
+    config.add_show_field 'publication_dates', label: 'Publication Date'
+    config.add_show_field 'main_call_number', label: 'Call number'
+    config.add_show_field 'isbn_numbers', label: 'ISBN'
 
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
@@ -145,7 +147,7 @@ class CatalogController < ApplicationController
     # solr request handler? The one set in config[:default_solr_parameters][:qt],
     # since we aren't specifying it otherwise.
 
-    config.add_search_field 'all_fields', label: 'All Fields'
+    config.add_search_field 'text', label: 'All Fields'
 
 
     # Now we see how to over-ride Solr request handler defaults, in this
@@ -161,21 +163,17 @@ class CatalogController < ApplicationController
       }
     end
 
-    config.add_search_field('author') do |field|
-      field.solr_parameters = {
-        'spellcheck.dictionary': 'author',
-        qf: '${author_qf}',
-        pf: '${author_pf}'
+    config.add_search_field('Author/Creator') do |field|
+      field.solr_parameters = { 'spellcheck.dictionary': 'creator' }
+      field.solr_local_parameters = {
+        qf: '${creator_qf}',
+        pf: '${creator_pf}'
       }
     end
 
-    # Specifying a :qt only to show it's possible, and so our internal automated
-    # tests can test it. In this case it's the same as
-    # config[:default_solr_parameters][:qt], so isn't actually neccesary.
     config.add_search_field('subject') do |field|
-      field.qt = 'search'
-      field.solr_parameters = {
-        'spellcheck.dictionary': 'subject',
+      field.solr_parameters = { 'spellcheck.dictionary': 'subject' }
+      field.solr_local_parameters = {
         qf: '${subject_qf}',
         pf: '${subject_pf}'
       }
@@ -185,10 +183,10 @@ class CatalogController < ApplicationController
     # label in pulldown is followed by the name of the SOLR field to sort by and
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
-    config.add_sort_field 'score desc, pub_date_si desc, title_si asc', label: 'relevance'
-    config.add_sort_field 'pub_date_si desc, title_si asc', label: 'year'
-    config.add_sort_field 'author_si asc, title_si asc', label: 'author'
-    config.add_sort_field 'title_si asc, pub_date_si desc', label: 'title'
+    config.add_sort_field 'score desc, title_sort asc', label: 'relevance'
+    config.add_sort_field 'publication_dates desc, title_sort asc', label: 'publication date'
+    config.add_sort_field 'creator_sort asc, title_sort asc', label: 'creator'
+    config.add_sort_field 'title_sort asc', label: 'title'
 
     # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -9,4 +9,11 @@ class SearchBuilder < Blacklight::SearchBuilder
   #   def add_custom_data_to_query(solr_parameters)
   #     solr_parameters[:custom] = blacklight_params[:user_value]
   #   end
+
+  self.default_processor_chain += [:filter_out_suppressed_records]
+
+  def filter_out_suppressed_records(solr_parameters)
+    solr_parameters[:fq] ||= []
+    solr_parameters[:fq] << 'suppressed:false'
+  end
 end
