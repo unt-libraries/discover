d75cb349f3e50cc56244337c23d86527dcc49181
jason-ellis <jason.ellis@unt.edu>
2019-06-25 13:42:28 -0500

    Initial book cover lookup & replace by isbn or oclc

---
+++ app/assets/javascripts/ui.js
@@ -0,0 +1,89 @@
+const has = Object.prototype.hasOwnProperty;
+const idTypes = {
+  isbn: 'isbnNumbers',
+  oclc: 'oclcNumbers',
+};
+
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
+    fn();
+  } else {
+    document.addEventListener('turbolinks:load', fn);
+  }
+}
+
+function docIDObject() {
+  const thumbnails = document.querySelectorAll('.document-thumbnail');
+  const docObject = {};
+  Object.keys(idTypes).forEach((idKey) => {
+    docObject[idKey] = {};
+  });
+  thumbnails.forEach((thumb) => {
+    Object.entries(idTypes).forEach(([idKey, dataID]) => {
+      const idString = thumb.dataset[dataID];
+      const bibString = thumb.dataset.bibId;
+      if (idString !== undefined) {
+        const idStripped = idString.replace(/[[\]"']+/g, '');
+        const idArray = idStripped.split(',');
+        idArray.forEach((id) => {
+          docObject[idKey][id] = { bib: bibString };
+        });
+      }
+    });
+  });
+  return docObject;
+}
+
+window.replaceImages = function (payload) {
+  const documentsEl = document.querySelector('#documents');
+  const docIDs = docIDObject();
+  Object.entries(payload).forEach(([bookKey, bookData]) => {
+    const [idType, id] = bookKey.split(':');
+    const docThumbEl = documentsEl.querySelector(`[data-bib-id="${docIDs[idType][id].bib}"]`);
+    if (has.call(bookData, 'thumbnail_url')) {
+      const thumbPlaceholder = docThumbEl.querySelector('.default');
+      if (thumbPlaceholder) {
+        const imgSrcZoom = bookData.thumbnail_url.replace(/zoom=./, 'zoom=1');
+        const imgSrc = imgSrcZoom.replace('&edge=curl', '');
+        console.log(`${imgSrc}`);
+        thumbPlaceholder.outerHTML = `<img class="thumbnail-loaded" src="${imgSrc}">`;
+      }
+    }
+  });
+};
+
+function docIDArray() {
+  const thumbnails = document.querySelectorAll('.document-thumbnail');
+  const docArray = [];
+  thumbnails.forEach((thumb) => {
+    Object.entries(idTypes).forEach(([idKey, dataID]) => {
+      const idString = thumb.dataset[dataID];
+      if (idString !== undefined) {
+        const idStripped = idString.replace(/[[\]"']+/g, '');
+        const idArray = idStripped.split(',');
+        idArray.forEach(id => docArray.push(`${idKey}:${id}`));
+      }
+    });
+  });
+  return docArray;
+}
+
+function docIDQueryString() {
+  const bibArray = docIDArray();
+  return bibArray.join(',');
+}
+
+function replaceBookCovers() {
+  const bibkeyQueryString = docIDQueryString();
+
+  if (bibkeyQueryString.length > 0) {
+    const scriptElement = document.createElement('script');
+    scriptElement.setAttribute('id', 'jsonScript');
+    scriptElement.setAttribute('src',
+      `https://books.google.com/books?bibkeys=${bibkeyQueryString}&jscmd=viewapi&callback=replaceImages`);
+    scriptElement.setAttribute('type', 'text/javascript');
+    document.documentElement.firstChild.appendChild(scriptElement);
+  }
+}
+
+ready(replaceBookCovers);
