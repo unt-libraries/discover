1e24df4a05c7421d2e670f3fb7ce62ac1ac556e9
jason-ellis <jason.ellis@unt.edu>
2019-07-31 10:43:35 -0500

    Force SSL in all environments except development

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module Discover
     # Loads site variables from config/config.yml. Used in templates like Rails.configuration.x.site['var']
     config.x.site = config_for(:config)
 
+    # Force SSL on all environments, can be overridden in environment config files.
+    config.force_ssl = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
