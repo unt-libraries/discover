5d4cc3024c98116f0798fccb1caa560a1e567375
Jason Ellis <jason.ellis@unt.edu>
2020-02-12 14:30:03 -0600

    Refactor big UI update function, add ability to map URLs to locations.

--- app/webpacker/src/javascripts/_availability.js
+++ app/webpacker/src/javascripts/_availability.js
@@ -1,5 +1,9 @@
 import moment from 'moment';
 
+const locationMapUrls = {
+  w3: 'https://library.unt.edu/assets/images/spaces/floor-maps/willis-3rd-floor.png',
+};
+
 function chunkArray(arr, chunkSize) {
   const chunkedArray = [];
   while (arr.length) {
@@ -12,63 +16,85 @@ function getItemsIDs() {
   const itemEls = document.querySelectorAll('[data-item-id]');
   const itemsArray = Array.from(itemEls);
 
-  const itemBibs = itemsArray.map(el => el.dataset.itemId.replace(/\D/g, ''));
+  const itemBibs = itemsArray.map((el) => el.dataset.itemId.replace(/\D/g, ''));
   return chunkArray(itemBibs, 50);
 }
 
 function findMissing(foundItems = [], allItems) {
-  const foundIDs = foundItems.map(el => el.id);
-  return allItems.filter(el => !foundIDs.includes(el));
+  const foundIDs = foundItems.map((el) => el.id);
+  return allItems.filter((el) => !foundIDs.includes(el));
+}
+
+function getLocationUrl(locationCode) {
+  if (Object.hasOwnProperty.call(locationMapUrls, locationCode)) {
+    return locationMapUrls[locationCode];
+  }
+  return false;
+}
+
+function updateStatusElement(itemEl, itemStatus) {
+  const availabilityEl = itemEl.querySelector('.blacklight-availability.result__value');
+
+  if (!itemStatus) {
+    availabilityEl.innerText = 'ASK AT SERVICE DESK';
+    return;
+  }
+
+  availabilityEl.dataset.statusCode = itemStatus.code;
+
+  // If the item is checked out
+  if (itemStatus.duedate) {
+    const dueDate = moment(itemStatus.duedate).format('MMM DD, YYYY');
+    availabilityEl.innerText = `Checked out\nDue ${dueDate}`;
+  } else {
+    availabilityEl.innerText = itemStatus.display;
+  }
+
+  // Show the Request column if this isn't an online only record
+  if (itemStatus.code !== 'w') {
+    const availTableBody = itemEl.parentNode;
+    const availTable = availTableBody.parentNode;
+    const requestEls = availTable.querySelectorAll('.blacklight-request.d-none');
+
+    if (requestEls) {
+      requestEls.forEach((node) => {
+        node.classList.remove('d-none');
+      });
+    }
+  }
+}
+
+function updateLocationElement(itemEl, itemLocation) {
+  const locationEl = itemEl.querySelector('.blacklight-location.result__value');
+
+  if (!itemLocation) {
+    return;
+  }
+
+  locationEl.dataset.locationCode = itemLocation.code;
+  const locationUrl = getLocationUrl(itemLocation.code);
+  if (locationUrl) {
+    locationEl.innerHTML = `<a href="${locationUrl}" target="_blank">${itemLocation.name}</a>`;
+  } else {
+    locationEl.innerText = itemLocation.name;
+  }
 }
 
 function updateUI(foundItems = [], missingItems = []) {
+  // Update elements for items returned by the API
   foundItems.forEach((item) => {
     const itemEl = document.querySelector(`[data-item-id='${item.id}']`);
 
-    // Update the location field if the API returned location data
-    if (item.location) {
-      const locationEl = itemEl.querySelector('.blacklight-location.result__value');
-      locationEl.dataset.locationCode = item.location.code;
-      locationEl.innerText = item.location.name;
-    }
-
-    // Update the status field whether or not the API returned status data
-    const availabilityEl = itemEl.querySelector('.blacklight-availability.result__value');
-    if (item.status) {
-      availabilityEl.dataset.statusCode = item.status.code;
-
-      // If the item is checked out
-      if (item.status.duedate) {
-        const dueDate = moment(item.status.duedate).format('MMM DD, YYYY');
-        availabilityEl.innerText = `Checked out\nDue ${dueDate}`;
-      } else {
-        availabilityEl.innerText = item.status.display;
-      }
-
-      // Show the Request column if this isn't an online only record
-      if (item.status.code !== 'w') {
-        const availTableBody = itemEl.parentNode;
-        const availTable = availTableBody.parentNode;
-        const requestEls = availTable.querySelectorAll('.blacklight-request.d-none');
-
-        if (requestEls) {
-          requestEls.forEach((node) => {
-            node.classList.remove('d-none');
-          });
-        }
-      }
-    } else {
-      availabilityEl.innerText = 'ASK AT SERVICE DESK';
-    }
+    updateStatusElement(itemEl, item.status);
+    updateLocationElement(itemEl, item.location);
   });
 
-  // Update the data for items that were not found by the API
+  // Update elements for items that were not found by the API
   missingItems.forEach((item) => {
     const itemEl = document.querySelector(`[data-item-id='${item}']`);
-    const locationEl = itemEl.querySelector('.blacklight-location.result__value');
-    const availabilityEl = itemEl.querySelector('.blacklight-availability.result__value');
-    availabilityEl.innerText = 'ASK AT SERVICE DESK';
-    console.log(`Item ${item.id} not returned by the API`);
+    updateStatusElement(itemEl);
+    updateLocationElement(itemEl);
+    console.log(`Item ${item} not returned by the API`);
   });
 }
 
