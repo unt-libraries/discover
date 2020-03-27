2d4216a64019d003ebcaf5cd355aa7e2a55a84b8
Jason Ellis <jason.ellis@unt.edu>
2020-03-27 10:57:14 -0500

    Move thumbnail logic into helper module

---
+++ app/helpers/thumbnail_helper.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module ThumbnailHelper
+
+  def render_thumbnail(document, opts = { counter: nil })
+    thumbnail_url = document[:thumbnail_url]
+    current_view = params[:action]
+
+    thumbnail_tag = content_tag(:div,
+                { class: "document-thumbnail text-center #{current_view}-thumbnail d-none d-lg-block #{'thumbnail-loaded' if thumbnail_url}"}) do
+      if thumbnail_url.blank?
+        svg_icon = "#{asset_pack_path('media/images/icons/regular.svg')}##{document.resource_type_icon}"
+        content_tag(:svg, { class: "img-fluid #{current_view}-icon icon" }) do
+          concat("<use xlink:href=\"#{svg_icon}\"></use>".html_safe)
+          concat("<span class=\"sr-only item-title\">#{document[:full_title]}</span>".html_safe)
+        end
+      else
+        image_tag(thumbnail_url, { class: 'image-fluid', alt: document[:full_title] })
+      end
+    end
+
+    return link_to_document(document, thumbnail_tag, opts) if current_view == 'index'
+    thumbnail_tag
+  end
+end
