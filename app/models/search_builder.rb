2f4742816f4a01e2dc8b738210f85e73171cc953
Jason Ellis <jason.ellis@unt.edu>
2020-06-05 10:54:28 -0500

    Limit facet requests on home page to only those needed

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -3,6 +3,7 @@
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
   include BlacklightAdvancedSearch::AdvancedSearchBuilder
+
   self.default_processor_chain += [:add_advanced_parse_q_to_solr, :add_advanced_search_to_solr]
 
   ##
@@ -13,10 +14,24 @@ class SearchBuilder < Blacklight::SearchBuilder
   #     solr_parameters[:custom] = blacklight_params[:user_value]
   #   end
 
-  self.default_processor_chain += [:filter_out_suppressed_records]
+  self.default_processor_chain += [:filter_out_suppressed_records, :only_home_facets]
 
   def filter_out_suppressed_records(solr_parameters)
     solr_parameters[:fq] ||= []
     solr_parameters[:fq] << 'suppressed:false'
+  end
+
+  def only_home_facets(solr_parameters)
+    return if search_parameters?
+    solr_parameters['facet.field'] = ["access_facet", "resource_type_facet", "media_type_facet", "collection_facet"]
+    solr_parameters['facet.pivot'] = []
+  end
+
+  ##
+  # Check if any search parameters have been set
+  # @return [Boolean]
+  def search_parameters?
+    !blacklight_params[:q].nil? || blacklight_params[:f].present? ||
+      blacklight_params[:search_field] == 'advanced'
   end
 end
