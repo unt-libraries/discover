5e49db72cdc2f9f12abc072a764cdadd17f5b306
Jason Ellis <jason.ellis@unt.edu>
2020-06-09 16:42:24 -0500

    Separate UNT and Discover SCSS, apply updates from library theme

--- app/webpacker/packs/vendor/font-awesome.js
+++ app/webpacker/packs/vendor/font-awesome.js
@@ -1,5 +1,6 @@
 import '../../src/javascripts/vendor/font-awesome/solid';
 import '../../src/javascripts/vendor/font-awesome/light';
+import '../../src/javascripts/vendor/font-awesome/brands';
 import '../../src/javascripts/vendor/font-awesome/fontawesome';
 
 FontAwesome.config.mutateApproach = 'sync';
