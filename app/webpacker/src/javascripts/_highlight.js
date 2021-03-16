4baed3c1dd127ab000fb369371e89db2ce5ee68d
Jason Ellis <jason.ellis@unt.edu>
2021-03-16 14:58:50 -0500

    Mark.js highlighting revision

--- app/webpacker/src/javascripts/_highlight.js
+++ app/webpacker/src/javascripts/_highlight.js
@@ -7,28 +7,19 @@ function highlightSearchTerms() {
   /**
    * Converts a user query string to two arrays:
    * @param {string} q
-   * @return {Object}
-   *   allTerms {Array} each phrase in quotes is one term and each word outside quotes is one term.
-   *   justWords {Array} ONLY the terms that occur ourside of quotes, i.e. single-word terms
+   * @param {Array} allStopwords
+   * @return {Array} all terms
    */
-  function queryToTerms(q) {
-    return q.split('"').reduce((env, term, i) => {
+  function queryToTerms(q, allStopwords) {
+    return q.split('"').reduce((allTerms, term, i) => {
       const insideQuotes = i % 2;
-      if (insideQuotes) {
-        const trimmed = term.trim();
-        if (trimmed) {
-          env.allTerms.push(trimmed);
-          if (trimmed.indexOf(' ') === -1) {
-            env.justWords.push(trimmed);
-          }
-        }
-      } else {
-        const words = term.trim().split(' ').filter((x) => x);
-        env.allTerms.push(...words);
-        env.justWords.push(...words);
+      const trimmed = term.trim();
+      const words = trimmed.split(' ').filter((x) => !!x);
+      if (insideQuotes && words.every((word) => allStopwords.indexOf(word) > -1)) {
+        return allTerms.concat(trimmed);
       }
-      return env;
-    }, { allTerms: [], justWords: [] });
+      return allTerms.concat(...words);
+    }, []);
   }
 
   /**
@@ -62,23 +53,18 @@ function highlightSearchTerms() {
           env.finalPhrases.push(...doStack(term, env.stopwordStack, false));
           env.stopwordStack = [];
         }
-        if (term.indexOf(' ') !== -1) {
-          env.userPhrases.push(term);
-        }
         env.previousMainTerm = term;
       }
       return env;
     }, {
       allStopwords,
       finalPhrases: [],
-      userPhrases: [],
       previousMainTerm: '',
       stopwordStack: [],
     });
     if (result.stopwordStack.length && result.previousMainTerm) {
       result.finalPhrases.push(...doStack(result.previousMainTerm, result.stopwordStack, true));
     }
-    result.finalPhrases.unshift(...result.userPhrases);
     return result.finalPhrases;
   }
 
@@ -86,16 +72,14 @@ function highlightSearchTerms() {
    * The main function to use to parse a user query.
    * @param {string} userQuery
    * @param {Array} allStopwords
-   * @return {Object}
-   *   phrases {Array} The list of phrases to highlight, from `termsToPhrases`.
-   *   justWords {Array }The list of single words to highlight, MINUS stopwords.
+   * @return {Array} all terms to find, does not include stopwords
    */
   function parseUserQuery(userQuery, allStopwords) {
-    const qterms = queryToTerms(userQuery.toLowerCase());
-    const allAsPhrase = qterms.allTerms.join(' ');
-    const phrases = [allAsPhrase].concat(termsToPhrases(qterms.allTerms, allStopwords));
-    const wordsNoStopwords = [...new Set(qterms.justWords)].filter((x) => allStopwords.indexOf(x) === -1);
-    return { phrases, justWords: wordsNoStopwords };
+    const qterms = queryToTerms(userQuery.toLowerCase(), allStopwords);
+    const allAsPhrase = qterms.join(' ');
+    const phrases = [allAsPhrase].concat(termsToPhrases(qterms, allStopwords));
+    const justTerms = qterms.filter((term) => allStopwords.indexOf(term) === -1);
+    return [...new Set(phrases.concat(justTerms))];
   }
 
   // Example vars that may need to be changed. These are used in both methods.
@@ -153,7 +137,7 @@ function highlightSearchTerms() {
     const instance = new Mark(document.querySelector('.card.item-more-details'));
 
     // Parse user query
-    const result = parseUserQuery(userQuery, myStopwords);
+    const termList = parseUserQuery(userQuery, myStopwords);
 
     // Remove previous marked elements and mark new phrases and words
     instance.unmark({
@@ -162,10 +146,8 @@ function highlightSearchTerms() {
            character classes that will match most punctuation and space, to give better
            highlighting/matching no matter what word delimiter is used in the query *and* the text.
         */
-        const termList = result.phrases.concat(result.justWords);
         const termRe = termsToRegExp(myPunctuation, termList);
         instance.markRegExp(termRe, {
-          acrossElements: true,
           className: 'markjs',
           exclude: ['.result__label'],
           ignoreGroups: 1,
