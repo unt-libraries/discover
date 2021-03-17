6272b2e766153db27751bd8ffc8af184a3e61c93
Jason Ellis <jason.ellis@unt.edu>
2021-03-17 13:59:36 -0500

    Update highlight code for better call number handling

--- app/webpacker/src/javascripts/_highlight.js
+++ app/webpacker/src/javascripts/_highlight.js
@@ -4,22 +4,75 @@ import Mark from 'mark.js/dist/mark.es6';
  * Uses mark.js to highlight search terms in Show view
  */
 function highlightSearchTerms() {
+  /** hyphenize: If a term includes any transitions between numbers and letters
+   * (with optional spaces), a separate hyphenated term is generated for each
+   * of these transitions.
+   *
+   * E.g.: `mt1000 c35` => ['mt-1000-c-35', 'mt-1000-c', 'mt-1000',
+   *                        '1000-c-35', '1000-c', 'c-35']
+   *
+   * Assuming we're handling interword punctuation so that these marks will
+   * match any interword delimiter OR word boundary (i.e., punctuation, space,
+   * or nothing) -- this should let us (mostly) mimic Solr's behavior for these
+   * kinds of terms. It should at least be good enough to highlight call numbers,
+   * SuDocs, and other formatted numbers, no matter how the user formatted the
+   * number.
+   * @param {string} term
+   * @return {Array} list of hyphenated terms generated from the input term
+   */
+  function hyphenize(term) {
+    const hyphenizerRe = /\s*(.*?(?:\p{N}(?=\s*\p{L})|\p{L}(?=\s*\p{N})))\s*/u;
+    return term.split(hyphenizerRe).reduce((env, part, i, original) => {
+      if (part) {
+        const subParts = part.split(' ');
+        if (env.partStack.length > 0) {
+          if ((part.indexOf(' ') > -1) || (i === original.length - 1)) {
+            env.partStack.push(subParts[0]);
+            env.partStack.forEach((val, j, originalStack) => {
+              const stackSlice = originalStack.slice(j);
+              while (stackSlice.length > 1) {
+                env.hyphenized.push(stackSlice.join('-'));
+                stackSlice.pop();
+              }
+            });
+            env.partStack = [];
+          }
+        }
+        env.partStack.push(subParts[subParts.length - 1]);
+      }
+      return env;
+    }, {
+      hyphenized: [],
+      partStack: [],
+    }).hyphenized;
+  }
+
   /**
    * Converts a user query string to two arrays:
    * @param {string} q
    * @param {Array} allStopwords
-   * @return {Array} all terms
+   * @return {Object}
+   *   cleanTerms {Array} list of terms from the user query, in query order
+   *   hyphenizedTerms {Array} list of hyphenated terms for e.g. parts of call numbers
    */
   function queryToTerms(q, allStopwords) {
-    return q.split('"').reduce((allTerms, term, i) => {
+    return q.split('"').reduce((env, term, i) => {
       const insideQuotes = i % 2;
       const trimmed = term.trim();
-      const words = trimmed.split(' ').filter((x) => !!x);
-      if (insideQuotes && words.every((word) => allStopwords.indexOf(word) > -1)) {
-        return allTerms.concat(trimmed);
+      if (trimmed) {
+        const words = trimmed.split(' ').filter((x) => !!x);
+        if (insideQuotes && words.every((word) => allStopwords.indexOf(word) > -1)) {
+          env.cleanTerms.push(trimmed);
+        } else {
+          env.cleanTerms.push(...words);
+        }
+        env.hyphenizedTerms.push(...hyphenize(trimmed));
       }
-      return allTerms.concat(...words);
-    }, []);
+      return env;
+    }, {
+      cleanTerms: [],
+      hyphenizedTerms: [],
+    });
   }
 
   /**
@@ -76,17 +129,17 @@ function highlightSearchTerms() {
    */
   function parseUserQuery(userQuery, allStopwords) {
     const qterms = queryToTerms(userQuery.toLowerCase(), allStopwords);
-    const allAsPhrase = qterms.join(' ');
-    const phrases = [allAsPhrase].concat(termsToPhrases(qterms, allStopwords));
-    const justTerms = qterms.filter((term) => allStopwords.indexOf(term) === -1);
-    return [...new Set(phrases.concat(justTerms))];
+    const allAsPhrase = qterms.cleanTerms.join(' ');
+    const phrases = [allAsPhrase].concat(termsToPhrases(qterms.cleanTerms, allStopwords));
+    const justTerms = qterms.cleanTerms.filter((term) => allStopwords.indexOf(term) === -1);
+    return [...new Set(qterms.hyphenizedTerms.concat(phrases).concat(justTerms))];
   }
 
   // Example vars that may need to be changed. These are used in both methods.
   // Array of stopwords for demonstration purposes.
   const myStopwords = ['a', 'an', 'the', 'is', 'in', 'on', 'of', 'to', 'from', 'and'];
   // Array of punctuation marks to ignore (used inside words and at word boundaries).
-  const myPunctuation = ":;.,-–—‒_(){}[]!'+=>".split('');
+  const myPunctuation = [..."/\\:;.,-–—‒_(){}[]!'+=><`~@#$%^&*"];
 
   function escapeStringForRe(str) {
     return str.replace(/[-[\]/{}()*+?.\\^$|]/g, '\\$&');
@@ -106,9 +159,9 @@ function highlightSearchTerms() {
       'oòóỏõọôồốổỗộơởỡớờợöøōOÒÓỎÕỌÔỒỐỔỖỘƠỞỠỚỜỢÖØŌ', 'rřRŘ', 'sšśșşSŠŚȘŞ', 'tťțţTŤȚŢ',
       'uùúủũụưừứửữựûüůūUÙÚỦŨỤƯỪỨỬỮỰÛÜŮŪ', 'yýỳỷỹỵÿYÝỲỶỸỴŸ', 'zžżźZŽŻŹ'];
     const escapedPunctuation = escapeStringForRe(punctuationArray.join(''));
-    const internalPunctRe = `[${escapedPunctuation}]*`;
-    const wordDelimitersRe = `[\\s${escapedPunctuation}]+`;
-    const finalTermList = terms.map((term) => term.split(new RegExp(wordDelimitersRe)).map((word) => word.split('').map((char) => {
+    const internalPunctRe = `[${escapedPunctuation}]`;
+    const wordDelimitersRe = `[\\s${escapedPunctuation}]`;
+    const finalTermList = terms.map((term) => term.split(new RegExp(`${wordDelimitersRe}+`)).map((word) => word.split('').map((char) => {
       let charRe = char;
       diacritics.every((dct) => {
         if (dct.indexOf(char) !== -1) {
@@ -118,10 +171,10 @@ function highlightSearchTerms() {
         return true;
       });
       return charRe;
-    }).filter((x) => !!x).join(internalPunctRe))
-      .filter((x) => !!x).join(wordDelimitersRe))
+    }).filter((x) => !!x).join(`${internalPunctRe}*`))
+      .filter((x) => !!x).join(`${wordDelimitersRe}*`))
       .filter((x) => !!x);
-    return new RegExp(`(^|${wordDelimitersRe})(${finalTermList.join('|')})(?=(${wordDelimitersRe}|$))`, 'i');
+    return new RegExp(`(^|${wordDelimitersRe}+)(${finalTermList.join('|')})(?=(${wordDelimitersRe}+|$))`, 'i');
   }
 
   function regExpMark() {
