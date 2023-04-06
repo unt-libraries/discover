5fb5bc96bd6be8f2cd1669563ba04c20979b9654
Jason Ellis <jason.ellis@unt.edu>
2023-04-06 13:05:35 -0500

    Add airbnb-typescript linting. Add jest for js tests.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,23 @@
 module.exports = {
-  extends: 'airbnb-base',
+  extends: [
+    'airbnb-base',
+  ],
   parser: '@typescript-eslint/parser',
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'airbnb-typescript/base',
+      ],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
+  ],
   plugins: [
     '@typescript-eslint',
   ],
+  root: true,
   ignorePatterns: ['node_modules/', 'public/', 'tmp/'],
   rules: {
     'func-names': [
