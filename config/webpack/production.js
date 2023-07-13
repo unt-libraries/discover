f1a04093aa92c9d186d0da8ec2e53954ea1c0bac
Jason Ellis <jason.ellis@unt.edu>
2023-07-13 15:13:23 -0500

    Try rule in production env to exclude tests

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,6 +3,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 const environment = require('./environment');
 
 // Ignore files in tests directory in production env
-environment.config.set('module.rules.exclude', /^(?!.*tests\/.*$).+\.[jt]sx?$/);
+environment.config.set('module.rules.exclude', {
+  test: /^(?!.*tests\/.*$).+\.[jt]sx?$/,
+  enforce: 'pre',
+  // exclude js, jsx, ts, and tsx files in the js directory
+  exclude: /\.(js|jsx|ts|tsx)?(\.erb)?$/,
+});
 
 module.exports = environment.toWebpackConfig();
