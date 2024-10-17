663d6161638e7918f9bbc3233c38891b2fa0339e
Jason Ellis <jason.ellis@unt.edu>
2024-10-17 10:21:30 -0500

    Forgot to add file to previous commit, rename spec from js to ts

--- spec/javascripts/_availability_buttons.spec.ts
+++ spec/javascripts/_availability_buttons.spec.ts
@@ -1,7 +1,25 @@
+import { enableFetchMocks } from 'jest-fetch-mock';
+import { serviceDeskData } from './__mocks__/service_desks';
+import { statusDescData } from './__mocks__/availability_statuses';
+import { locationData } from './__mocks__/availability_locations';
+
+enableFetchMocks();
+jest.mock('../../app/webpacker/src/javascripts/data/service_desks', () => ({
+  serviceDeskData,
+}));
+jest.mock('../../app/webpacker/src/javascripts/data/availability_statuses', () => ({
+  statusDescData,
+}));
+jest.mock('../../app/webpacker/src/javascripts/data/availability_locations', () => ({
+  locationData,
+}));
+
 import { checkAvailability } from '../../app/webpacker/src/javascripts/_availability_buttons';
 
+import mocked = jest.mocked;
+
 describe('checkAvailability', () => {
-  it('should remove duplicate buttons', () => {
+  it('should remove duplicate buttons', async () => {
     document.body.innerHTML = `
       <div id="documents">
         <div class="document" data-item-id="1">
@@ -19,9 +37,12 @@ describe('checkAvailability', () => {
       </div>
     `;
 
-    checkAvailability();
+    await checkAvailability();
 
     const item1 = document.querySelector('[data-item-id="1"]');
+    if (!item1) {
+      throw new Error('Item 1 not found');
+    }
     const itemButtons = item1.querySelectorAll('.result__field');
     expect(itemButtons.length).toEqual(1);
   });
