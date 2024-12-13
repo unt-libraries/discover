0c2fa5b945529b479551d7f5f82673b1bebc5f29
Jason Ellis <jason.ellis@unt.edu>
2024-12-13 10:30:34 -0600

    Host fontawesome icons, load crucial SVG inline

---
+++ app/webpacker/packs/icons.js
@@ -0,0 +1,6 @@
+import '../vendor/fontawesome/js/brands.min.js';
+import '../vendor/fontawesome/js/duotone.min.js';
+import '../vendor/fontawesome/js/solid.min.js';
+import '../vendor/fontawesome/js/light.min.js';
+import '../vendor/fontawesome/js/custom-icons.min.js';
+import '../vendor/fontawesome/js/fontawesome.min.js';
