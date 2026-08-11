d1c296e0da7b8aab1ef8a49adaa27d9a92aabf64
Jason Ellis <jason.ellis@unt.edu>
2026-08-11 14:41:21 -0500

    Change Turnstile skip condition to use X-Forwarded-For because of load balancer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,10 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
+  config.action_dispatch.trusted_proxies = [
+    IPAddr.new("129.120.90.173")
+  ]
+
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 
